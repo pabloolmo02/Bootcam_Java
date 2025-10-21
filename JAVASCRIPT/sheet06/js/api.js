398a8016cf308cfd544bde7a743a3667a39792fd
unknown <pabloolmolopez9@gmail.com>
2025-10-21 19:44:56 +0200

    feat: Pokedex conectada a api

---
+++ JAVASCRIPT/sheet06/js/api.js
@@ -0,0 +1,31 @@
+// api.js
+async function loadPokemon() {
+  try {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100');
+    const data = await response.json();
+
+    const promises = data.results.map(async p => {
+      const res = await fetch(p.url);
+      const details = await res.json();
+      const speciesRes = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${details.id}/`);
+      const speciesData = await speciesRes.json();
+
+      return {
+        id: details.id,
+        name: details.name,
+        image: details.sprites.front_default,
+        types: details.types.map(t => t.type.name),
+        evolves_from: speciesData.evolves_from_species
+          ? speciesData.evolves_from_species.name
+          : null
+      };
+    });
+
+    allPokemon = await Promise.all(promises);
+    renderPokemon(allPokemon); // <-- función definida en render.js
+  } catch (error) {
+	const pokedex = document.getElementById('pokedex'); 
+    pokedex.innerHTML = `<p>Error al cargar Pokémon 😢</p>`;
+    console.error(error);
+  }
+}
---
+++ JAVASCRIPT/sheet06/js/mainn.js
@@ -0,0 +1,12 @@
+// main.js
+let allPokemon = [];
+const searchBox = document.querySelector('.search-box');
+const searchButton = document.getElementById('Buscar');
+
+searchBox.addEventListener('input', filterPokemon);
+searchButton.addEventListener('click', filterPokemon);
+searchBox.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') filterPokemon();
+});
+
+loadPokemon(); // ← función de api.js
