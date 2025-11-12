53dd0b18f5e3b226682f9e818994c7ddd0f65a45
Pablo Olmo López <pabloolmolopez9@gmail.com>
2025-11-12 18:55:49 +0100

    Feat: Ex01 completed

---
+++ JAVA/Sheet03/ex01/Main.java
@@ -0,0 +1,10 @@
+package Sheet03.ex01;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Producto producto1 = new Producto("Tuercas(10mm)", 0.1, 5236522);
+        producto1.mostrarDatos();
+    }
+    
+}
